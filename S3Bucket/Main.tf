5f81fa6fea9b185adf7543f165476431f86db624
sanjusanjay7 <131652504+sanjusanjay7@users.noreply.github.com>
2023-06-09 08:39:43 +0530

    Add files via upload

---
+++ S3Bucket/Main.tf
@@ -0,0 +1,22 @@
+## backend data for terraform
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "remote" {
+  hostname = "app.terraform.io"  
+  organization = "SonixLabs"
+
+    workspaces {
+      name = "AWSBackup"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
